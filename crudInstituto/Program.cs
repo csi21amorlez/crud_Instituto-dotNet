b997ad12baf3d4f217adf03b4d04fda0e4f80dc4
DESKTOP-QMGOQCG\CSI21-Amorlez <csi21-amorlez@colegioaltair.net>
2022-11-03 11:13:12 +0100

    Done 031122

--- crudInstituto/Program.cs
+++ crudInstituto/Program.cs
@@ -1,7 +1,13 @@
+using DALcrudInstituto.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ejemploDBFirstContext>(o => o.UseNpgsql(builder.Configuration.GetConnectionString("PostgreSQL")));
+
+
 
 var app = builder.Build();
 
